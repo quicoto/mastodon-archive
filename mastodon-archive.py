1d8e208495d61964ddd9147a0d8a22c755cbfd8f
Ricard Torres <ricard@ricard.dev>
2025-12-02 09:42:35 +0100

    Force CSS update

--- mastodon-archive.py
+++ mastodon-archive.py
@@ -100,7 +100,7 @@ header = """<!DOCTYPE html>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>%s Mastodon archive</title>
-  <link rel="stylesheet" href="styles.css?ver=3.0.0">
+  <link rel="stylesheet" href="styles.css?ver=3.1.0">
   <meta name="robots" content="noindex">
 </head>
 <body>
